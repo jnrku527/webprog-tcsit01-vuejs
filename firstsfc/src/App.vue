9aa0347643d8fa95602a075c32b26b41eb958045
Jin Cyril Baydo <152479532+jnrku527@users.noreply.github.com>
2025-11-03 01:08:12 +0000

    added Supabase client.js

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,21 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const instruments = ref([])
+
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+
+onMounted(() => {
+   getInstruments()
+})
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
 </template>
-
-<style scoped></style>
