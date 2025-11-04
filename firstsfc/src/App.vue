2e3c1135ebd00ae91b46e91f6c5556778b580eb8
Jin Cyril Baydo <152479532+jnrku527@users.noreply.github.com>
2025-11-04 10:46:17 +0000

    replaced app.vue and main.js and added cmponents folder

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,11 +1,11 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Hello WEBPROG TCSIT01 world!</h1>
+  <h2>Food</h2>
+  <food-item/>
+  <food-item2/> 
 </template>
 
-<style scoped></style>
+<script></script>
+
+<style>
+</style>
